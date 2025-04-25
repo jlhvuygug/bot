d32cfcfc7e8c0f33e23320daac983d395a477169
jlhvuygug <hhjkjhjnvcg@gmail.com>
2025-04-25 09:10:35 +0500

    myBot

---
+++ bot.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// TO‘LIQ tokenni shu yerga yozing:
+const token = '8060564822:AAGh5KAmYdXuoN2_fvidNpKjx3A33dNlJZA'; // bu faqat misol!
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  console.log(`Yangi xabar: ${text} (chat ID: ${chatId})`);
+
+  bot.sendMessage(chatId, `Salom, siz manga shunday yozdingiz: ${text}`);
+});
